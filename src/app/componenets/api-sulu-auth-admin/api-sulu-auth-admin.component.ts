0b72ba0664add96dc975c9da8923e0f11b173daa
Elmarnissi soufiane <soufiane.elmarnissi@edu.uiz.ac.ma>
2025-01-10 14:48:11 +0000

    createion de api-sulu-auth-admin.component.ts

---
+++ src/app/componenets/api-sulu-auth-admin/api-sulu-auth-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-sulu-auth-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './api-sulu-auth-admin.component.html',
+  styleUrl: './api-sulu-auth-admin.component.css'
+})
+export class ApiSuluAuthAdminComponent {
+
+}
